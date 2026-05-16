c77ad609b8d583e7767acab17c12e23d0231515c
= <=>
2026-05-16 19:03:45 -0300

    first commit

---
+++ src/main/java/br/com/alura/Screenmatch/model/DadosSerie.java
@@ -0,0 +1,11 @@
+package br.com.alura.Screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(
+        @JsonAlias("Title") String titulo,
+        @JsonAlias("totalSeasons") Integer temporadas,
+        @JsonAlias("imdbRating") String avaliacao
+) {}
